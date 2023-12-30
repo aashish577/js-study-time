421916790a5b427b7b7d1fc2e568ca6f0b88a93d
Aashish Acharya <er.aashishacharya@gmail.com>
2023-12-30 18:27:51 +0545

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 12345
+let accountEmail = "aashish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kathmandu"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+
+accountEmail ="achary@gmail.com"
+
+accountPassword = "3124"
+
+accountCity = 'Pokhara'
+
+/*
+Prefer not to use var
+because of of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataypes.js
@@ -0,0 +1,36 @@
+"use strict";//treated all JS code as newer version
+
+// alert("Hello") //We are using nodejs, not browser
+
+console.log(3 + 3); //code readability should be high
+
+console.log("aashish")
+
+let name = "aashish" //string datatype
+let age = 18 //number datatype
+let isLoggedIn = false //boolena data type
+
+
+//PRIMITIVE DATATYPES//
+
+//number => 2^53
+//bigint =>
+//string => ""
+//boolean => true/false
+
+/*null => It is a standalon value and a datatype, 
+representaion of empty value, 
+NULL is a object*/
+
+// undefined => when the value is not defined or assigned
+//symbol => to find the uniqeness 
+
+
+//OBJECT
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
+
+
+
+
