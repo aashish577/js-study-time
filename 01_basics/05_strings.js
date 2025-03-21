d27b1d235a51e32f166f92c5415e4d704ad08afe
Aashish Acharya <er.aashishacharya@gmail.com>
2025-03-21 15:54:32 +0545

    commit

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,37 +1,60 @@
-const name = "aashish-acharya"
-const num = 50
+// const name = "aashish-acharya"
+// const num = 50
 
-// console.log(name + num)
+// // console.log(name + num)
 
-console.log(`Hello my name is ${name}and my number is ${num}`);
+// console.log(`Hello my name is ${name}and my number is ${num}`);
 
-const gameName = new String('aashish')
+// const gameName = new String('aashish')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length)
-console.log(gameName.toUpperCase())
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf('s'))
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('s'))
 
-const newString = gameName.substring(0, 4)
+// const newString = gameName.substring(0, 4)
 
-console.log(newString);
+// console.log(newString);
 
-const anotherString = gameName.slice(-7, 3)
+// const anotherString = gameName.slice(-7, 3)
 
-console.log(anotherString)
+// console.log(anotherString)
 
-const newString1 = '    aashish   '
-console.log(newString1);
-console.log(newString.trim());
+// const newString1 = '    aashish   '
+// console.log(newString1);
+// console.log(newString.trim());
 
-const url = "https://aashish%@gmail.com"
+// const url = "https://aashish%@gmail.com"
 
-console.log(url.replace('%@','-'))
+// console.log(url.replace('%@','-'))
 
-console.log(url.includes('aashish'))
+// console.log(url.includes('aashish'))
+
+
+// const name = "Aashish"
+// const rollCount = "191201"
+
+// console.log(name+ rollCount + "value");
+
+// console.log (`hello my name is ${name} and my roll count is ${rollCount}`);
+
+
+// const myName = new String('Aashish-Acharya')
+
+// // console.log(myName[0]);
+
+// // console.log(myName.__proto__);
+// console.log(myName.charAt('1'));
+
+// const newString = myName.substring(0, 4);
+// console.log(newString);
+
+const myName = "  Aashish  ";
+console.log(myName);
+console.log(myName.trim());
 
 
 
---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,33 @@
+// const score = 400;
+
+// console.log(score)
+
+// const balance = new Number(500.987)
+
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(4));
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString());
+
+//++++++++++++++Maths-----------------//
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const min = 10;
+const max = 20;
+
+console.log (Math.random() * (max - min + 1))
+
+
+
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min) 
