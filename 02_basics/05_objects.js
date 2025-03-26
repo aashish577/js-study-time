b60773dc9abf4d7f1a786b6d7b84d0f5a188f217
Aashish Acharya <er.aashishacharya@gmail.com>
2025-03-26 16:05:32 +0545

    json

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,33 @@
+////////// destructuring 
+
+const course = {
+    coursename: "Javascript",
+    price: "1000",
+    instructor: "youtube"
+
+}
+
+// const{instructor: teacher} = course
+// // console.log(instructor);
+// console.log(teacher);
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "microsoft")
+
+
+///// API
+
+// {
+//     name: "Aashish",
+//     courseName: "javascript",
+//     Price: "999"
+// }
+
+[
+    {},
+    {},
+    {}
+]
