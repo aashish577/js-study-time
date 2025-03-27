0a0d54401af135d5ef5fab493e32714164c74336
Aashish Acharya <er.aashishacharya@gmail.com>
2025-03-27 13:31:53 +0545

    nestedscopes

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -8,12 +8,59 @@ if (true) {
 
     const b = 20
 
-    console.log("Inner:", a)
+    // console.log("Inner:", a)
     // var c = 30
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one (){
+    const username = "Aashish"
+
+    function two (){
+        const website = "Youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website)
+
+    two()
+}
+
+one()
+
+if (true){
+    const username = "Aashish"
+
+    if(username === "Aashish"){
+        const website = "Youtube"
+        // console.log(username + website);
+        
+    }
+
+    // console.log(website);
+    // console.log(username);
+}
+
+// console.log(username);
+
+/////========== INTERESTING CONCEPT
+console.log(addone (5))
+function addone (num){
+    return num + 1
+}
+
+
+
+const addTwo = function (num){
+    return num + 2
+}
+
+console.log(addTwo(5))      
+
+
