f9752a4f6845305b8762eb91c93c0c94e3cea815
Aashish Acharya <er.aashishacharya@gmail.com>
2025-03-27 23:38:08 +0545

    controlflow

---
+++ 04_control_flow/1.js
@@ -0,0 +1,62 @@
+// if
+// = operator assignment 
+
+// const isUserloggedIn = true
+
+// const temperature = 20
+// if ( temperature === 50){
+//     console.log("less than 50");
+    
+// } else {
+//     console.log("Temperature is greater than 50")
+// }
+
+// console.log("Execute")
+
+// const score = 101
+
+// if(score > 100){
+//     const power = "run"
+//     console.log(`User power : ${ power}`);
+    
+// }
+
+
+// const balance = 1000
+
+// if (balance < 500) {
+//     console.log("less than 500");
+    
+// }else if (balance < 750){
+//     console.log("less than 750");
+
+// }else if(balance < 900){
+//     console.log("less than 900");
+
+// }else{
+//     console.log("less than 1200");
+
+// }
+
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard){
+//     console.log("Allow to buy courses");
+    
+// }else{
+//     console.log("couldnot login")
+// }
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("Allow to buy courses");
+}
+
+
+{/* < less than, > greater than, <= less than equal to , >= less than equal to , == check if equal , != not equal .  */}
+
+// === checks type as well, !==
