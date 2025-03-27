02817c000694e2d4e8ce99f0ee68b7054a03b11f
Aashish Acharya <er.aashishacharya@gmail.com>
2025-03-27 13:10:00 +0545

    scopes1

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,19 @@
+
+let c = 150
+
+let a = 300
+
+if (true) {
+    let a = 10
+
+    const b = 20
+
+    console.log("Inner:", a)
+    // var c = 30
+}
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
